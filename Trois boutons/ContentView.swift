d3805189f3367fed1cbafebf1435c1e8ced2c363
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-28 14:54:21 +0100

    Initial Commit

---
+++ Trois boutons/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trois boutons
+//
+//  Created by  Ixart on 28/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trois boutons/Trois_boutonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trois_boutonsApp.swift
+//  Trois boutons
+//
+//  Created by  Ixart on 28/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Trois_boutonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
