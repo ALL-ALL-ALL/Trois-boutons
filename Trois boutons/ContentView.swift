16d0a9c42dfd21ccf03de600cfc7dee65962600f
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-02-28 15:32:00 +0100

    les trois bouton de chaque couleur il manque l'action des boutons

--- Trois boutons/ContentView.swift
+++ Trois boutons/ContentView.swift
@@ -9,16 +9,120 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
-}
+        HStack {
+            
+            Button {
+                
+                
+            } label: {
+                
+                HStack{
+
+                    ZStack{
+                    
+
+                        Rectangle()
+                            .frame(width: 120, height: 60)
+                            .cornerRadius(20)
+                            .foregroundColor(.red)
+                        HStack{
+                            Image(systemName: "paintbrush.pointed.fill")
+                                .foregroundColor(.black)
+
+                            Text("Red")
+                                .font(.system(size: 25))
+                                .foregroundColor(.black)
+                                .bold()
+            
+                        } // fin hsatck
+                    } // fin Zstack
+
+                } // fin hstack
+            } // fin label
+            
+            
+            
+            
+            Button {
+                
+                
+            } label: {
+                
+                HStack{
+
+                    ZStack{
+                    
+
+                        Rectangle()
+                            .frame(width: 120, height: 60)
+                            .cornerRadius(20)
+                            .foregroundColor(.blue)
+                        HStack{
+                            Image(systemName: "paintbrush.pointed.fill")
+                                .foregroundColor(.black)
+
+                            Text("Blue")
+                                .font(.system(size: 25))
+                                .foregroundColor(.black)
+                                .bold()
+            
+                        } // fin hsatck
+                    } // fin Zstack
+
+                } // fin hstack
+            } // fin label
+            
+            
+            
+            
+            
+            Button {
+                
+                
+            } label: {
+                
+                HStack{
+
+                    ZStack{
+                    
+
+                        Rectangle()
+                            .frame(width: 120, height: 60)
+                            .cornerRadius(20)
+                            .foregroundColor(.green)
+                        HStack{
+                            Image(systemName: "paintbrush.pointed.fill")
+                                .foregroundColor(.black)
+
+                            Text("Grenn")
+                                .font(.system(size: 25))
+                                .foregroundColor(.black)
+                                .bold()
+            
+                        } // fin hsatck
+                    } // fin Zstack
+
+                } // fin hstack
+            } // fin label
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        } // FIN HSTACK
+        
+    } // FIN BODY
+} // FIN STRUCT
 
 #Preview {
     ContentView()
 }
+
+
