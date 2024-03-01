479257a3d8691291d409df4c02ecdf44fb6ed7aa
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-01 02:17:43 +0100

    obliger de tout refaire car il y  avait un probleme de stack

--- Trois boutons/ContentView.swift
+++ Trois boutons/ContentView.swift
@@ -8,117 +8,91 @@
 import SwiftUI
 
 struct ContentView: View {
+
+
     var body: some View {
-        HStack {
-            
-            Button {
-                
-                
-            } label: {
-                
-                HStack{
+        VStack{
 
-                    ZStack{
+            HStack {
+
+                        Button {
+
+                        } label: {
+                            ZStack{
+                                Rectangle()
+                                    .foregroundColor(.red)
+                                    .cornerRadius(10)
+                                    .frame(width: 120, height: 60)
+
+                                HStack{
+                                    Image(systemName: "paintbrush.pointed.fill")
+                                        .foregroundColor(.black)
+                                    Text("Red")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(.black)
+                                        .font(.headline)
+                                } // HSTACK
+                            }// fin zstack
+                        } // fin label
+                
+                
+                        Button {
+
+                        } label: {
+                            ZStack{
+                                Rectangle()
+                                    .foregroundColor(.blue)
+                                    .cornerRadius(10)
+                                    .frame(width: 120, height: 60)
+
+                                HStack{
+                                    Image(systemName: "paintbrush.pointed.fill")
+                                        .foregroundColor(.black)
+                                    Text("Blue")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(.black)
+                                        .font(.headline)
+                                } // HSTACK
+                            }// fin zstack
+                        } // fin label
+                
+                
+                
+                
+                
+                        Button {
+
+                        } label: {
+                            ZStack{
+                                Rectangle()
+                                    .foregroundColor(.green)
+                                    .cornerRadius(10)
+                                    .frame(width: 120, height: 60)
+
+                                HStack{
+                                    Image(systemName: "paintbrush.pointed.fill")
+                                        .foregroundColor(.black)
+                                    Text("Grenn")
+                                        .font(.system(size: 25))
+                                        .foregroundColor(.black)
+                                        .font(.headline)
+                                } // HSTACK
+                            }// fin zstack
+                        } // fin label
+                
+                
+                
+                
+                
+                
+                
+   
+            } // HSTACK
                     
-
-                        Rectangle()
-                            .frame(width: 120, height: 60)
-                            .cornerRadius(20)
-                            .foregroundColor(.red)
-                        HStack{
-                            Image(systemName: "paintbrush.pointed.fill")
-                                .foregroundColor(.black)
-
-                            Text("Red")
-                                .font(.system(size: 25))
-                                .foregroundColor(.black)
-                                .bold()
-            
-                        } // fin hsatck
-                    } // fin Zstack
-
-                } // fin hstack
-            } // fin label
-            
-            
-            
-            
-            Button {
-                
-                
-            } label: {
-                
-                HStack{
-
-                    ZStack{
-                    
-
-                        Rectangle()
-                            .frame(width: 120, height: 60)
-                            .cornerRadius(20)
-                            .foregroundColor(.blue)
-                        HStack{
-                            Image(systemName: "paintbrush.pointed.fill")
-                                .foregroundColor(.black)
-
-                            Text("Blue")
-                                .font(.system(size: 25))
-                                .foregroundColor(.black)
-                                .bold()
-            
-                        } // fin hsatck
-                    } // fin Zstack
-
-                } // fin hstack
-            } // fin label
-            
-            
-            
-            
-            
-            Button {
-                
-                
-            } label: {
-                
-                HStack{
-
-                    ZStack{
-                    
-
-                        Rectangle()
-                            .frame(width: 120, height: 60)
-                            .cornerRadius(20)
-                            .foregroundColor(.green)
-                        HStack{
-                            Image(systemName: "paintbrush.pointed.fill")
-                                .foregroundColor(.black)
-
-                            Text("Grenn")
-                                .font(.system(size: 25))
-                                .foregroundColor(.black)
-                                .bold()
-            
-                        } // fin hsatck
-                    } // fin Zstack
-
-                } // fin hstack
-            } // fin label
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-            
-        } // FIN HSTACK
-        
+        } // VSTACK
+  
     } // FIN BODY
+    
 } // FIN STRUCT
 
 #Preview {
